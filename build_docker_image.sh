879bc818d9c7d19699705078c9312e5ec80dd76d
haitham.abou-houly <haitham.abou-houly@digital.homeoffice.gov.uk>
2022-08-19 15:01:24 +0100

    update image

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-
 set +x
-
 # Attach host network to allow DNS resolution via local DNS resolution service
 docker build --network=host \
   -t pandas-kafka:dev1 \
--- run_docker_image.sh
+++ run_docker_image.sh
@@ -5,5 +5,5 @@ set +x
 # Attach host network to allow DNS resolution via local DNS resolution service
 docker run \
   --rm \
-  -ti crt-s3-backup:dev1 \
+  -ti pandas-kafka:dev1 \
   $@
